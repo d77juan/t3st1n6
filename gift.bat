c7b6480aafef1521ab2cef5d41b789e2d36d2c0e
d77juan <79436534+d77juan@users.noreply.github.com>
2021-06-16 17:03:18 -0700

    Update gift.bat

--- gift.bat
+++ gift.bat
@@ -1,1 +1,1 @@
-for /F %%x in (computers.txt) do c:\windows\temp\psexec.exe -accepteula -d -u vonese\administrator  -p Zxcv7890! notepad %%x 
+for /F %%x in (c:\windows\temp\computers.txt) do c:\windows\temp\psexec.exe -accepteula -d -u vonese\administrator  -p Zxcv7890! notepad %%x 
